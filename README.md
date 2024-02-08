586c215f0abed30a5b4a68a39469f963d3ebaed8
fzvincent <yuliangkun@outlook.com>
2024-02-08 11:21:13 -0700

    md

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
-# FL_AoR
+## Latency Aware Semi-synchronous Client Selection and Model Aggregation for Wireless Federated Learning
 
+![wfl.jpg](fig%2Fwfl.jpg)
+### Abstract
+Federated learning (FL) is a collaborative machine learning (ML) framework particularly 1
+suited for ML models requiring numerous training samples, such as Convolutional Neural Networks 2
+(CNNs), Recurrent Neural Networks (RNNs), and Random Forest, in the context of various applica- 3
+tions, e.g., next-word prediction and eHealth. FL involves various clients participating in the training 4
+process by uploading their local models to an FL server in each global iteration. The server aggregates 5
+these local models to update a global model. The traditional FL process may encounter bottlenecks, 6
+known as the straggler problem, where slower clients delay the overall training time. This paper in- 7
+troduces the Latency awarE Semi-synchronous client Selection and mOdel aggregation for federated 8
+learNing (LESSON) method. LESSON allows clients to participate at different frequencies: faster 9
+clients contribute more frequently, thereby mitigating the straggler problem and expediting conver- 10
+gence. Moreover, LESSON provides a tunable trade-off between model accuracy and convergence 11
+rate by setting varying deadlines. Simulation results show that LESSON outperforms two baseline 12
+methods, namely FedAvg and FedCS, in terms of convergence speed and maintains higher model 13
+accuracy as compared to FedCS.
+### Semi-synchronous FL Scheduling (LESSON)
+![LESSON_sch.jpg](fig%2FLESSON_sch.jpg)
+### Non Independent and Identically Distribution
+The non-iid sample distribution on clients is simulated with a Dirichlet distribution with β. Larger β makes the distribution more evenly.
+![data_dis.jpg](fig%2Fdata_dis.jpg)
+https://en.wikipedia.org/wiki/Independent_and_identically_distributed_random_variables
+### Code
 env: tensorflow-federated 
 
-main is modified from "https://www.tensorflow.org/federated/tutorials/building_your_own_federated_learning_algorithm"
+[main.py](main.py) is modified from https://www.tensorflow.org/federated/tutorials/building_your_own_federated_learning_algorithm
+
+### Paper
+https://www.mdpi.com/1999-5903/15/11/352
+#### Citation
+@Article{fi15110352,
+AUTHOR = {Yu, Liangkun and Sun, Xiang and Albelaihi, Rana and Yi, Chen},
+TITLE = {Latency-Aware Semi-Synchronous Client Selection and Model Aggregation for Wireless Federated Learning},
+JOURNAL = {Future Internet},
+VOLUME = {15},
+YEAR = {2023},
+NUMBER = {11},
+ARTICLE-NUMBER = {352},
+URL = {https://www.mdpi.com/1999-5903/15/11/352},
+ISSN = {1999-5903},
+DOI = {10.3390/fi15110352}
+}
--- result/fig5_8/plot0717.py
+++ result/fig5_8/plot0717.py
@@ -1,20 +1,19 @@
-import numpy as np
+import itertools
 import math
-from matplotlib import pyplot as plt
 import pickle
+
+import numpy as np
+from matplotlib import pyplot as plt
+
 # open a file, where you stored the pickled data
 import var
-import itertools
-import copy
-
-
 
 grid = []
-model='cifar10'
+model = 'cifar10'
 methods_with_repetion = ['aog']
 methods_without_repetion = ['avg']
 utiers = [0, 1, 2, 3]
-repetion=3
+repetion = 3
 
 grid.append({
     'betaValues': [10],
@@ -49,17 +48,17 @@ grid.append({
     'model': [model]
 })
 
-setting_list=[]
+setting_list = []
 for i, onegrid in enumerate(grid):
-    setting_list += [dict(zip(onegrid.keys(),values)) for values in itertools.product(*onegrid.values())]
-setting_list1=setting_list
+    setting_list += [dict(zip(onegrid.keys(), values)) for values in itertools.product(*onegrid.values())]
+setting_list1 = setting_list
 for i, d1 in reversed(list(enumerate(setting_list))):
-    for j, d2 in enumerate(setting_list[0:i-1]):
-        if i!=0 and d1==d2:
+    for j, d2 in enumerate(setting_list[0:i - 1]):
+        if i != 0 and d1 == d2:
             setting_list.pop(i)
 
-data=[]
-data_sl=[]
+data = []
+data_sl = []
 
 # for i in range(len(setting_list)):
 #     file = open('3kforall_9_'+str(i)+'.pkl', 'rb')
@@ -68,16 +67,14 @@ data_sl=[]
 #     data_sl=data_sl+[pickle.load(file)]
 
 file = open('3kforall_9.pkl', 'rb')
-data=pickle.load(file)
+data = pickle.load(file)
 # print(len(data))
-data_sl=pickle.load(file)
-
+data_sl = pickle.load(file)
 
 file = open('mnist0728.pkl', 'rb')
-data=data+pickle.load(file)
+data = data + pickle.load(file)
 # print(len(data))
-data_sl=data_sl+pickle.load(file)
-
+data_sl = data_sl + pickle.load(file)
 
 # newdata=[]
 # newlist=[]
@@ -103,58 +100,78 @@ data_sl=data_sl+pickle.load(file)
 # print(1)
 
 
-sizeH=9
-sizeV=2.5
-VALID_RANGE=var.VALID_RANGE
-NUM_VALID=var.NUM_VALID
-TEST_frequency=var.TEST_frequency
-fedavglatency=var.fedavglatency
-betaValues=[0.1,1,10]
+sizeH = 9
+sizeV = 2.5
+VALID_RANGE = var.VALID_RANGE
+NUM_VALID = var.NUM_VALID
+TEST_frequency = var.TEST_frequency
+fedavglatency = var.fedavglatency
+betaValues = [0.1, 1, 10]
 
 # figure 4 mnist and cifar
-mnist_range=201
-mnist_valid_range=range(1,2002,10)
-tierTimes=var.tierTimes
-fig4, axs = plt.subplots(ncols=4,sharey=True,figsize=[sizeH,sizeV],tight_layout=True)
-axs[0].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=0,model='cifar10'))],label='LESSON')
-axs[0].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=1,model='cifar10'))],label='FedCS')
-axs[0].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=1,methods='avg',model='cifar10'))],label='FedAvg') # beta=1
+mnist_range = 201
+mnist_valid_range = range(1, 2002, 10)
+tierTimes = var.tierTimes
+fig4, axs = plt.subplots(ncols=4, sharey=True, figsize=[sizeH, sizeV], tight_layout=True)
+axs[0].plot(VALID_RANGE,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=0, model='cifar10'))],
+            label='LESSON')
+axs[0].plot(VALID_RANGE,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=1, model='cifar10'))],
+            label='FedCS')
+axs[0].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=1, methods='avg', model='cifar10'))],
+            label='FedAvg')  # beta=1
 axs[0].set(xlabel='Iterations \n(a) CIFAR-10', ylabel='Test accuracy')
 
 # axs[1].plot(VALID_RANGE[0:mnist_range], validNumAvg1_mnist[1,0:mnist_range],label='FedAvg') #1 refers to beat=1
 # axs[1].plot(VALID_RANGE[0:mnist_range], valid5sTierAvg_mnist[1, 0, 0:mnist_range], label='FedCS')
 # axs[1].plot(VALID_RANGE[0:mnist_range], valid5sTierAvg_mnist[1, 1, 0:mnist_range], label='LESSON')
-axs[1].plot(mnist_valid_range, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=0,model='mnist'))][:mnist_range],label='LESSON')
-axs[1].plot(mnist_valid_range, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=1,model='mnist'))][:mnist_range],label='FedCS')
-axs[1].plot(mnist_valid_range, data[data_sl.index(dict(betaValues=1,methods='avg',model='mnist'))][:mnist_range],label='FedAvg')
+axs[1].plot(mnist_valid_range,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=0, model='mnist'))][:mnist_range],
+            label='LESSON')
+axs[1].plot(mnist_valid_range,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=1, model='mnist'))][:mnist_range],
+            label='FedCS')
+axs[1].plot(mnist_valid_range, data[data_sl.index(dict(betaValues=1, methods='avg', model='mnist'))][:mnist_range],
+            label='FedAvg')
 axs[1].set(xlabel='Iterations\n(b) MNIST', ylabel='Test accuracy')
 
 # f5fit = math.ceil(NUM_VALID*(deadline / fedavglatency))
 # f4fit_cifar=math.ceil(20/fedavglatency*var.NUM_VALID)
-f4fit_cifar= math.ceil(NUM_VALID*(20 / fedavglatency))
+f4fit_cifar = math.ceil(NUM_VALID * (20 / fedavglatency))
 
-axs[2].plot(np.arange(NUM_VALID) *20*TEST_frequency, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=0,model='cifar10'))],  label='LESSON')
-axs[2].plot(np.arange(NUM_VALID) *20*TEST_frequency, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=1,model='cifar10'))], label='FedCS')
-axs[2].plot(np.arange(f4fit_cifar)*fedavglatency*TEST_frequency,data[data_sl.index(dict(betaValues=1,methods='avg',model='cifar10'))][:f4fit_cifar], label='FedAvg')
+axs[2].plot(np.arange(NUM_VALID) * 20 * TEST_frequency,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=0, model='cifar10'))],
+            label='LESSON')
+axs[2].plot(np.arange(NUM_VALID) * 20 * TEST_frequency,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=1, model='cifar10'))],
+            label='FedCS')
+axs[2].plot(np.arange(f4fit_cifar) * fedavglatency * TEST_frequency,
+            data[data_sl.index(dict(betaValues=1, methods='avg', model='cifar10'))][:f4fit_cifar], label='FedAvg')
 axs[2].set(xlabel='Time (s)\n(c) CIFAR-10', ylabel='Test accuracy')
 tick_positions = np.arange(0, 100002, 50000)
-tick_labels = [f"{int(val/1000)},000" for val in tick_positions]
-tick_labels[0]=0
-axs[2].set_xticks(tick_positions,tick_labels)
+tick_labels = [f"{int(val / 1000)},000" for val in tick_positions]
+tick_labels[0] = 0
+axs[2].set_xticks(tick_positions, tick_labels)
 # axs[2].ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-f4fit_mnist=math.ceil(20/fedavglatency*mnist_range)
+f4fit_mnist = math.ceil(20 / fedavglatency * mnist_range)
 # axs[3].plot(np.arange(f4fit_mnist)*validNumTimeAvg1_mnist[1,0]*TEST_frequency,validNumAvg1_mnist[1,0:f4fit_mnist], label='FedAvg')
 # axs[3].plot(np.arange(mnist_range)*tierTimes[1]*TEST_frequency, valid5sTierAvg_mnist[1, 0, 0:mnist_range], label='FedCS')
 # axs[3].plot(np.arange(mnist_range)*tierTimes[1]*TEST_frequency, valid5sTierAvg_mnist[1, 1, 0:mnist_range],  label='LESSON')
-axs[3].plot(np.arange(mnist_range)*20*TEST_frequency, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=0,model='mnist'))][:mnist_range],  label='LESSON')
-axs[3].plot(np.arange(mnist_range)*20*TEST_frequency, data[data_sl.index(dict(betaValues=1,tierTimes=20,methods='aog',utiers=1,model='mnist'))][:mnist_range], label='FedCS')
-axs[3].plot(np.arange(f4fit_mnist)*fedavglatency*TEST_frequency,data[data_sl.index(dict(betaValues=1,methods='avg',model='mnist'))][:f4fit_mnist], label='FedAvg')
+axs[3].plot(np.arange(mnist_range) * 20 * TEST_frequency,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=0, model='mnist'))][:mnist_range],
+            label='LESSON')
+axs[3].plot(np.arange(mnist_range) * 20 * TEST_frequency,
+            data[data_sl.index(dict(betaValues=1, tierTimes=20, methods='aog', utiers=1, model='mnist'))][:mnist_range],
+            label='FedCS')
+axs[3].plot(np.arange(f4fit_mnist) * fedavglatency * TEST_frequency,
+            data[data_sl.index(dict(betaValues=1, methods='avg', model='mnist'))][:f4fit_mnist], label='FedAvg')
 axs[3].set(xlabel='Time (s)\n(d) MNIST', ylabel='Test accuracy')
 tick_positions = np.arange(0, 80001, 40000)
-tick_labels = [f"{int(val/1000)},000" for val in tick_positions]
-tick_labels[0]=0
-axs[3].set_xticks(tick_positions,tick_labels)
+tick_labels = [f"{int(val / 1000)},000" for val in tick_positions]
+tick_labels[0] = 0
+axs[3].set_xticks(tick_positions, tick_labels)
 
 # axs[3].ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 for i in range(4):
@@ -164,20 +181,23 @@ fig4.show()
 fig4.savefig('f4.pdf')
 
 ## figure 5 different methods with different betas by iterations
-fig5, axs = plt.subplots(ncols=3,sharey=True,figsize=[sizeH,sizeV],tight_layout=True)
-for k,beta in enumerate(betaValues):
-    #for j,deadline in enumerate(tierTimes[1:2]):
-    deadline=var.tierTimes[0]
-    axs[k].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=beta,tierTimes=deadline,methods='aog',utiers=0,model='cifar10'))], label='LESSON')
-    axs[k].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=beta,tierTimes=deadline,methods='aog',utiers=1,model='cifar10'))], label='FedCS')
-    axs[k].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=beta,methods='avg',model='cifar10'))], label='FedAvg')
+fig5, axs = plt.subplots(ncols=3, sharey=True, figsize=[sizeH, sizeV], tight_layout=True)
+for k, beta in enumerate(betaValues):
+    # for j,deadline in enumerate(tierTimes[1:2]):
+    deadline = var.tierTimes[0]
+    axs[k].plot(VALID_RANGE, data[
+        data_sl.index(dict(betaValues=beta, tierTimes=deadline, methods='aog', utiers=0, model='cifar10'))],
+                label='LESSON')
+    axs[k].plot(VALID_RANGE, data[
+        data_sl.index(dict(betaValues=beta, tierTimes=deadline, methods='aog', utiers=1, model='cifar10'))],
+                label='FedCS')
+    axs[k].plot(VALID_RANGE, data[data_sl.index(dict(betaValues=beta, methods='avg', model='cifar10'))], label='FedAvg')
     axs[k].grid()
     axs[k].set(xlabel='Iterations\n(' + chr(97 + k) + ') \u03B2=' + str(beta), ylabel='Test accuracy')
 axs[0].legend()
 fig5.savefig('f50.pdf')
 fig5.savefig('f50.jpg')
 fig5.show()
-
 
 # fig5, axs = plt.subplots(ncols=3,sharey=True,figsize=[sizeH,sizeV],tight_layout=True)
 # for k,beta in enumerate(betaValues):
@@ -195,21 +215,26 @@ fig5.show()
 
 
 ## figure 5_1 different methods with different betas by time
-fig5_1, axs = plt.subplots(ncols=3,sharey=True,figsize=[sizeH,sizeV],tight_layout=True)
+fig5_1, axs = plt.subplots(ncols=3, sharey=True, figsize=[sizeH, sizeV], tight_layout=True)
 tick_positions = np.arange(0, 100001, 50000)
-tick_labels = [f"{int(val/1000)},000" for val in tick_positions]
-tick_labels[0]=0
-for k,beta in enumerate(betaValues):
-    for j in range(0,1):
-        deadline=var.tierTimes[j]
-        f5fit = math.ceil(NUM_VALID*(deadline / fedavglatency))
+tick_labels = [f"{int(val / 1000)},000" for val in tick_positions]
+tick_labels[0] = 0
+for k, beta in enumerate(betaValues):
+    for j in range(0, 1):
+        deadline = var.tierTimes[j]
+        f5fit = math.ceil(NUM_VALID * (deadline / fedavglatency))
         # axs[k].plot(VALID_RANGE, validTierAvg[j, k, 1, :], label='LESSON')
         # axs[k].plot(VALID_RANGE, validTierAvg[j, k, 0, :], label='FedCS')
         # axs[k].plot(VALID_RANGE, validNumAvg[j, k, :], label='FedAvg')
-        axs[k].plot(np.arange(NUM_VALID)*deadline*TEST_frequency, data[data_sl.index(dict(betaValues=beta,tierTimes=deadline,methods='aog',utiers=0,model='cifar10'))], label='LESSON')
-        axs[k].plot(np.arange(NUM_VALID)*deadline*TEST_frequency, data[data_sl.index(dict(betaValues=beta,tierTimes=deadline,methods='aog',utiers=1,model='cifar10'))], label='FedCS')
-        axs[k].plot(np.arange(f5fit)*fedavglatency*TEST_frequency, data[data_sl.index(dict(betaValues=beta,methods='avg',model='cifar10'))][:f5fit], label='FedAvg')
-        axs[k].set_xticks(tick_positions,tick_labels)
+        axs[k].plot(np.arange(NUM_VALID) * deadline * TEST_frequency, data[
+            data_sl.index(dict(betaValues=beta, tierTimes=deadline, methods='aog', utiers=0, model='cifar10'))],
+                    label='LESSON')
+        axs[k].plot(np.arange(NUM_VALID) * deadline * TEST_frequency, data[
+            data_sl.index(dict(betaValues=beta, tierTimes=deadline, methods='aog', utiers=1, model='cifar10'))],
+                    label='FedCS')
+        axs[k].plot(np.arange(f5fit) * fedavglatency * TEST_frequency,
+                    data[data_sl.index(dict(betaValues=beta, methods='avg', model='cifar10'))][:f5fit], label='FedAvg')
+        axs[k].set_xticks(tick_positions, tick_labels)
     axs[k].grid()
     axs[k].set(xlabel='Time (s)\n(' + chr(97 + k) + ') \u03B2=' + str(beta), ylabel='Test accuracy')
 axs[0].legend()
@@ -219,12 +244,13 @@ fig5_1.savefig('f5_1.jpg')
 fig5_1.show()
 
 ## figure 7 compare fedaog with different tierTimes by iterations
-fig7, axs = plt.subplots(ncols=2,sharey=True,figsize=[sizeH,sizeV],tight_layout=True)
-for k,beta in enumerate(betaValues[0:2]):
-    for j,deadline in enumerate(var.tierTimes):
+fig7, axs = plt.subplots(ncols=2, sharey=True, figsize=[sizeH, sizeV], tight_layout=True)
+for k, beta in enumerate(betaValues[0:2]):
+    for j, deadline in enumerate(var.tierTimes):
         axs[k].plot(VALID_RANGE,
-                    data[setting_list.index(dict(betaValues=beta,tierTimes=deadline,utiers=0,methods='aog',model='cifar10'))],
-                    label='\u03C4='+str(deadline)+'s')
+                    data[setting_list.index(
+                        dict(betaValues=beta, tierTimes=deadline, utiers=0, methods='aog', model='cifar10'))],
+                    label='\u03C4=' + str(deadline) + 's')
     axs[k].grid()
     axs[k].set(xlabel='Iterations\n(' + chr(97 + k) + ') \u03B2=' + str(beta), ylabel='Test accuracy')
 axs[0].legend()
@@ -233,14 +259,15 @@ fig7.savefig('f7.jpg')
 fig7.show()
 
 ## figure 7_1 compare fedaog with different tierTimes by time
-fig7_1, axs = plt.subplots(ncols=2,sharey=True,figsize=[sizeH,sizeV],tight_layout=True)
-for k,beta in enumerate(betaValues[0:2]):
-    for j,deadline in enumerate(var.tierTimes):
-        #f7fig=math.ceil(NUM_VALID/2**(j))
-        f7fit = math.ceil(NUM_VALID*(var.tierTimes[0] / deadline))
-        axs[k].plot(np.arange(f7fit)*deadline*TEST_frequency,
-                    data[setting_list.index(dict(betaValues=beta,tierTimes=deadline,utiers=0,methods='aog',model='cifar10'))][:f7fit],
-                    label='\u03C4='+str(deadline)+'s')
+fig7_1, axs = plt.subplots(ncols=2, sharey=True, figsize=[sizeH, sizeV], tight_layout=True)
+for k, beta in enumerate(betaValues[0:2]):
+    for j, deadline in enumerate(var.tierTimes):
+        # f7fig=math.ceil(NUM_VALID/2**(j))
+        f7fit = math.ceil(NUM_VALID * (var.tierTimes[0] / deadline))
+        axs[k].plot(np.arange(f7fit) * deadline * TEST_frequency,
+                    data[setting_list.index(
+                        dict(betaValues=beta, tierTimes=deadline, utiers=0, methods='aog', model='cifar10'))][:f7fit],
+                    label='\u03C4=' + str(deadline) + 's')
         axs[k].grid()
         axs[k].set(xlabel='Time (s)\n(' + chr(97 + k) + ') \u03B2=' + str(beta), ylabel='Test accuracy')
 axs[0].legend()
